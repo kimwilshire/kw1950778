b00ee867e2a84426f09f9b90a7401828649bc390
Kim Wilshire <kim.wilshire@yahoo.com>
2016-01-07 20:47:17 -0800

    git push origin master

---
+++ Hmwk/HelloWrld/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 7, 2016, 6:37 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World" << endl;
+    return 0;
+}
+

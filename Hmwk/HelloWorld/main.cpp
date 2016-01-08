174f11165bb068619f9d0df68120e80af5128898
Kim Wilshire <kim.wilshire@yahoo.com>
2016-01-07 20:57:58 -0800

    updated

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Kim Wilshire
+ *
+ * Created on January 7, 2016, 6:37 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  
+    cout << "Hello World" << endl;
+    return 0;
+}
+
